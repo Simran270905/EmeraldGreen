1ea9288b07455eedb8e45ea99c0e2687b040564b
Simran270905 <simrankadamkb12@gmail.com>
2025-12-17 22:04:49 +0530

    file changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
  import Hero from "../components/Main";
 import Events from "../components/WeddingEvent";
 import RSVP from "../components/Rsvp";
-import Venue from "../components/venue";
-import Footer from "../components/footer";
-import Couple from "../components/couple";
+import Venue from "../components/Venue";
+import Footer from "../components/Footer";
+import Couple from "../components/Couple";
 import Story from "../components/Story";
 
 
